adb0c273168dd16f4c9161cbdb1c4ee6deabd93f
shegsteham <oluwasegunadepoju@gmail.com>
2018-08-31 13:03:03 +0100

    [Fixed #160097527] Allow user see list of available fast foods
    - arrange list with most recent additions appearing first
    - add interractivity with Javascript (cart value increments at every cick)
    - add styling with css
    
    [Fixed #160097527]

---
+++ UI/js/menu.js
@@ -0,0 +1,10 @@
+var addToCartBtn = document.getElementById('addToCartBtn');
+var cartWeight = document.getElementById('cartWeight');
+var cart = cartWeight.innerHTML;
+var click = document.querySelectorAll('#addToCartBtn')
+
+click.forEach(btn => btn.addEventListener('click', function(){
+    cart = Number(cart);
+    cart += 1;
+    cartWeight.innerHTML = cart;
+}));
