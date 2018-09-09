75ee2e4c168ca1898c947afef60c75bb5d7a17bb
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-09 12:47:56 +0100

    Create array/object data structure

---
+++ data/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    orders:[
+        {
+            id: 0,
+            food: 'peperony pizza',
+            price: 10,
+            quantity: 10,
+            orderStatus: 'uncompleted'
+        }
+    ]
+}
