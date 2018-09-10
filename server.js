49b154e0a22d9786e19dd406caeff3f3eb7a932e
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-10 11:58:24 +0100

    Update sever.js port to process.env.PORT || 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 let app = express()
 app.use (logger('common'))
