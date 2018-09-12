1b16a7b0293714aeea953296fffcd53eb793992b
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-12 14:37:37 +0100

    [fixed #160429282] implement strictly ES6 algorithms

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // Imports
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './routes/index';
+
+//port declaration
 const port = process.env.PORT || 5000;
 
-// Instantiations
-let app = express();
+// Instantiate Express
+const app = express();
 
-//Middlewares
+//instanciate imported middlewares
 app.use (logger('common'));
 app.use(bodyParser.json());
 
@@ -19,7 +21,7 @@ app.post('/api/v1/orders', routes.orders.addOrder);
 app.put('/api/v1/orders/:orderId', routes.orders.updateOrder);
 app.delete('/api/v1/orders/:orderId', routes.orders.deleteOrder);
 
-// Bootup
+// Set listener to port for API queries
 app.listen(port,  () => {
     console.log(`server running on port ${port}`);
   });
