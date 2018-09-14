da362d90d967dd89deeebde238d915e3787a114c
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-14 11:36:50 +0100

    Model database as class with sub  methods

--- data/database.js
+++ data/database.js
@@ -1,11 +1,41 @@
-let orders= [
-        {
-            orderId: 0,
-            food: 'peperony pizza',
-            price: 10,
-            quantity: 10,
-            orderStatus: 'uncompleted',
-            userAddress: "Address"
-        }
-    ];
-    export default orders;
+class Orders {
+    constructor() {
+        this.database = [
+            {
+                orderId: 0,
+                food: 'peperony pizza',
+                price: 10,
+                quantity: 10,
+                orderStatus: 'uncompleted',
+                userAddress: "Address"
+            }
+        ]
+    }
+
+    getAllOrder() {
+        let value = this.database;
+        return value;
+    }
+
+    returnOrder(index) {
+        let value = this.database[index];
+        return value;
+    }
+
+    pushOrder(newOrder) {
+        this.database.push(newOrder);
+        return this.database;
+    }
+
+    putOrder(index, newOrder) {
+        this.database[index] = newOrder;
+        return this.database;
+    }
+
+    spliceOrder(index) {
+        this.database.splice(index, 1);
+        return this.database;
+    }
+
+}
+export default Orders;
