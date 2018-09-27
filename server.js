7fb6ee4d6e0fdce17486148aa77a2b4b47705967
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-27 19:51:36 +0100

    Implement Router as a middleware
    - All routes using app.xxx("/", do.x) was removed and substituted with the Router middleware
    
    [#160798708]

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import routes from './routes/index';
+import router from './routes/orders';
 
 //port declaration
 const port = process.env.PORT || 5000;
@@ -17,13 +17,7 @@ const message = ()=>{
 //instanciate imported middlewares
 app.use (logger('common'));
 app.use(bodyParser.json());
-
-// Routes
-app.get('/api/v1/orders', routes.orders.getOrders);
-app.get('/api/v1/orders/:orderId', routes.orders.getOrder);
-app.post('/api/v1/orders', routes.orders.addOrder);
-app.put('/api/v1/orders/:orderId', routes.orders.updateOrder);
-app.delete('/api/v1/orders/:orderId', routes.orders.deleteOrder);
+app.use(router);
 
 // Set listener to port for API queries
 app.listen(port, message());
