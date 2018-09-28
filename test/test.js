a8014f98a48fe1ae0096d9a58abdedfca6892d7a
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-28 03:24:24 +0100

    Add more test queries to validate API response
    - Added more .expect() queries to validate the output data type, content and Status Code.
    - Imported test test cases to compare the API response
    - Imported test cases as an input data to the API
    
    [#160798708]

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../server';
+import * as test from '../model/order-model';
 
 // Tests for the POST Route
 describe('Validate POST Route', () => {
@@ -7,79 +8,79 @@ describe('Validate POST Route', () => {
         it('should be valid /api/v1/orders', (end) => {
             request(app).post('/api/v1/orders')
                 .type("JSON")
-                .send({
-                    "orderId": 1,
-                    "food": 'fufu',
-                    "quantity": 10,
-                    "price": 10,
-                    "orderStatus": "uncompleted",
-                    "userAddress": "address"
+                .send(test.fullOrder)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'Order Sent Successfully';
                 })
                 .expect(201, end)
         });
     });
     describe('Check POST input for Error', () => {
-        it('should return Error code 400 if no data was passed', (end) => {
+        it('should return Error status code 400 if no data was passed', (end) => {
             request(app).post('/api/v1/orders')
                 .type('JSON')
-                .expect(400, end);
-        });
-        it('Should Return Error code 400 if food name is missing', (end) => {
-            request(app).post('/api/v1/orders')
-                .type('JSON')
-                .send({
-                    "orderId": 1,
-                    "quantity": 10,
-                    "price": 10,
-                    "orderStatus": "uncompleted",
-                    "userAddress": "address"
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
-        it('should return Error code 400 if price is missing', (end) => {
+        it('Should Return Error status code 400 if food name is missing', (end) => {
             request(app).post('/api/v1/orders')
                 .type('JSON')
-                .send({
-                    "orderId": 1,
-                    "food": 'fufu',
-                    "quantity": 10,
-                    "orderStatus": "uncompleted",
-                    "userAddress": "address"
+                .send(test.voidFood)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
-        it('should return Error code 400 if quantity is missing', (end) => {
+        it('should return Error status code 400 if price is missing', (end) => {
             request(app).post('/api/v1/orders')
                 .type('JSON')
-                .send({
-                    "orderId": 1,
-                    "food": 'fufu',
-                    "price": 10,
-                    "orderStatus": "uncompleted",
-                    "userAddress": "address"
+                .send(test.voidPrice)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
-        it('should return Error code 400 if orderStatus is missing', (end) => {
+        it('should return Error status code 400 if quantity is missing', (end) => {
             request(app).post('/api/v1/orders')
                 .type('JSON')
-                .send({
-                    "orderId": 1,
-                    "food": 'fufu',
-                    "quantity": 10,
-                    "price": 10,
-                    "userAddress": "address"
+                .send(test.voidQuantity)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
-        it("should return statuscose 400 if food name is missing", (end) => {
-            request(app).post("/api/v1/orders").type('JSON').send({
-                "orderId": 1,
-                "food": 'fufu',
-                "quantity": 10,
-                "price": 10,
-                "orderStatus": "uncompleted"
-            }).expect(400, end);
+        it('should return Error status code 400 if orderStatus is missing', (end) => {
+            request(app).post('/api/v1/orders')
+                .type('JSON')
+                .send(test.voidOrderStatus)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
+                })
+                .expect(400, end);
+        });
+        it("should return status code 400 if food name is missing", (end) => {
+            request(app).post("/api/v1/orders")
+                .type('JSON')
+                .send(test.voidFood)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
+                })
+                .expect(400, end);
         });
     });
 });
@@ -89,15 +90,26 @@ describe('Validate GET Route', () => {
     it('should be valid route /api/v1/orders returns 200 (orders retrieved successfully)', (end) => {
         request(app).get('/api/v1/orders')
             .expect("Content-type", /json/)
+            .expect(function (res) {
+                res.body.success = 'true';
+                res.body.Status  = "Orders retrieved successfully";
+                res.body.orders = test.databaseOrders;
+            })
             .expect(200, end)
     });
     it('should be valid route /api/v1/orders/0 returns 200 (orders retrieved successfully)', (end) => {
         request(app).get('/api/v1/orders/0')
             .expect("Content-type", /json/)
+            .expect(function (res) {
+                res.body.success = 'true';
+                res.body.Status  = "Orders retrieved successfully";
+                res.body.orders = test.firstOrder;
+            })
             .expect(200, end)
     });
     it('should return error 404(order not found in the database) if database does not have data at that location', (end) => {
         request(app).get('/api/v1/orders/2000000')
+            .expect('Content-Type', /json/)
             .expect(404, end)
     });
 })
@@ -107,13 +119,9 @@ describe('Validate PUT Route', () => {
     describe("when Correct PUT Query is supplied", () => {
         it('should be valid route /api/v1/orders/0 editted successfully returning status code 200(Update Successful)', (end) => {
             request(app).put('/api/v1/orders/0')
-                .type('JSON').send({
-                    "orderId": 0,
-                    "food": 'pizza',
-                    "quantity": 10,
-                    "price": 9500,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
+                .type('JSON').send(test.fullOrder)
+                .expect(function (res) {
+                    res.body.Status  = "Update successful";
                 })
                 .expect(200, end)
         });
@@ -122,48 +130,44 @@ describe('Validate PUT Route', () => {
         it("should return 400(Bad request, when food is missing in the input data)", (end) => {
             request(app).put('/api/v1/orders/0')
                 .type('JSON')
-                .send({
-                    "orderId": 0,
-                    "quantity": 10,
-                    "price": 9500,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
+                .send(test.voidFood)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
         it("should return 400(Bad request, when quantity is missing in the input data)", (end) => {
             request(app).put('/api/v1/orders/0')
                 .type('JSON')
-                .send({
-                    "orderId": 0,
-                    "food": 'pizza',
-                    "price": 9500,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
+                .send(test.voidQuantity)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
         it("should return 400(Bad request, when userAddress is missing in the input data)", (end) => {
             request(app).put('/api/v1/orders/0')
                 .type('JSON')
-                .send({
-                    "orderId": 0,
-                    "food": 'pizza',
-                    "quantity": 10,
-                    "price": 9500,
-                    "orderStatus": "completed",
+                .send(test.voidUserAddress)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
         it("should return 400(Bad request, when price is missing in the input data)", (end) => {
             request(app).put('/api/v1/orders/0')
                 .type('JSON')
-                .send({
-                    "orderId": 0,
-                    "food": 'pizza',
-                    "quantity": 10,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
+                .send(test.voidPrice)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(400, end);
         });
@@ -171,18 +175,16 @@ describe('Validate PUT Route', () => {
             request(app).put('/api/v1/orders/0')
                 .type('JSON')
                 .send({})
+                .expect('Content-Type', /json/)
                 .expect(400, end);
         });
-        it("should return 404(Previous order not found, when orderId is undefined)", (end) => {
+        it("should return 400(Previous order not defined, when orderId is undefined)", (end) => {
             request(app).put('/api/v1/orders/')
                 .type('JSON')
-                .send({
-                    "orderId": 0,
-                    "food": 'pizza',
-                    "quantity": 10,
-                    "price": 9500,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
+                .send(test.fullOrder)
+                .expect(function (res) {
+                    res.body.Status = 'unsuccessful';
+                    res.body.Status.toLowerCase();
                 })
                 .expect(404, end);
         });
@@ -191,14 +193,8 @@ describe('Validate PUT Route', () => {
         it("should return 410(Previous order not found, when memory has no data to modify)", (end) => {
             request(app).put('/api/v1/orders/300')
                 .type('JSON')
-                .send({
-                    "orderId": 300,
-                    "food": 'pizza',
-                    "quantity": 10,
-                    "price": 9500,
-                    "orderStatus": "completed",
-                    "userAddress": "address"
-                })
+                .send(test.invalidOrderId)
+                .expect('Content-Type', /json/)
                 .expect(410, end);
         });
     });
@@ -209,13 +205,23 @@ describe("Validate Delete Route", () => {
     describe("When orderId is correctly supplied", () => {
         it('should return StatusCode 200(Order successfully deleted)', (end) => {
             request(app).delete('/api/v1/orders/1')
-                .expect(200,end)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.success = "true";
+                    res.body.Status = "Order deleted successfuly";
+                })
+                .expect(200, end)
         });
     });
     describe("When item can not be found in the database", () => {
         it('should return statusCode of 404(Order not found)', (end) => {
             request(app).delete('/api/v1/orders/300')
-                .expect(404,end)
+                .expect('Content-Type', /json/)
+                .expect(function (res) {
+                    res.body.success = "false";
+                    res.body.Status = "Order Not Found in the Database";
+                })
+                .expect(404, end)
         });
     });
 });
