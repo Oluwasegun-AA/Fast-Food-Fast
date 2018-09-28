e140def35c2c19cc2c63738315c87e77685dc4fb
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-28 03:19:59 +0100

    Resolve syntax error

--- middleware/validation.js
+++ middleware/validation.js
@@ -9,7 +9,7 @@ export default (req, res, next)=> {
     if (!(req.body.price)) {error.Price_Error ='Price is Required';}
     if (!(req.body.quantity)) {error.Quantity_Error = 'Quantity is Required';}
     if (!(req.body.orderStatus)) {error.Order_status_Error = 'Order Status is Required';}
-    if (!(req.body.userAddress)){error.Address_Error = 'User Address is Required'}
+    if (!(req.body.userAddress)){error.Address_Error = 'User Address is Required';}
     if ((Object.keys(error).length) > 0){ return res.status(400).send({Status: 'unsuccessful', Error_Log: error});}
     next();
 }
