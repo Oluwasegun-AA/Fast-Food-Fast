0e995be7a2c27564079beb6434fd3a9b332c04e6
shegsteham <oluwasegunadepoju@gmail.com>
2018-09-29 10:40:10 +0100

    Chore(API) Validate price and quantity
    - Valudation was added for price and quantity such that inly integers can be allowed
    
    [#160851995]

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,6 +5,9 @@
 */
 export default (req, res, next)=> {
     let error = {};
+    let numbers = /[0-9]|[0-9][0-9]/g;
+    if (!((req.body.price).match(numbers))) {  return res.status(400).send({Status: 'unsuccessful', Error_Log: 'Bad Request, Price is not Valid'});}
+    if (!((req.body.quantity).match(numbers))) {  return res.status(400).send({Status: 'unsuccessful', Error_Log: 'Bad Request, Quantity is not Valid'});}
     if (!(req.body.food)) {error.Food_Error = 'Food Name is Required';}
     if (!(req.body.price)) {error.Price_Error ='Price is Required';}
     if (!(req.body.quantity)) {error.Quantity_Error = 'Quantity is Required';}
