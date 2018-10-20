58ae9b8cb368762cc1c169a53751d7da6bf54334
shegsteham <oluwasegunadepoju@gmail.com>
2018-10-20 17:21:55 +0100

    Update user route

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,25 +1,28 @@
 //Import statements
 import express from 'express';
-import Controller from '../controllers/users'
-import Validate from '../middleware/usersValidation'
+import Controller from '../controllers/users';
+import Validate from '../middleware/usersValidation';
+import {verifyUserToken} from '../middleware/verifyToken';
 const router = express.Router();
 
 //create an instance of the request validation middlewares
 let validate = Validate.Validation;
-let validateUserId = Validate.userIdValidation
-let postvalidation = Validate.PostIdValidation
+let validateUserId = Validate.userIdValidation;
+let postvalidation = Validate.PostIdValidation;
 
 //creates an instance of the imported controller class
-const controller = new Controller(); 
+const controller = new Controller();
 
 // Routes
-router.get('/api/v1/users', controller.getUsers);
-router.get('/api/v1/users/:userId', validateUserId, controller.getUser);
-router.post('/api/v1/users',validate, controller.addUser);
-router.post('/api/v1/users/:userId', postvalidation,validate);
-router.put('/api/v1/users', validateUserId);
-router.put('/api/v1/users/:userId', validateUserId, validate, controller.updateUser);
-router.delete('/api/v1/users/:userId', validateUserId, controller.deleteUser);
-router.delete('/api/v1/users',  validateUserId);
+router.get('/users', controller.getUsers);
+router.get('/users/:userId', validateUserId, controller.getUser);
+router.post('/auth/login', validate,verifyUserToken,controller.login);
+router.post('/auth/signup', validate, controller.signup);
+router.post('/users', validate, verifyUserToken,validate, controller.addUser);
+router.post('/users/:userId', verifyUserToken,postvalidation);
+router.put('/users', validateUserId);
+router.put('/users/:userId', validateUserId, validate, controller.updateUser);
+router.delete('/users/:userId', validateUserId, controller.deleteUser);
+router.delete('/users',  validateUserId);
 
 export default router;
