101e31330519f8c88fd8ab84595a970b29655845
shegsteham <oluwasegunadepoju@gmail.com>
2018-10-20 17:05:14 +0100

    feat(API) Update user route
    - Add signup route /api/v1/auth/signup
    - Add login route /api/v1/auth/login
    
    [#161362134]

--- API/controllers/users.js
+++ API/controllers/users.js
@@ -1,8 +1,58 @@
 //Import statments
-import model from '../model/user-model';
-import database from '../db/Index'
+import model from '../seed/populate';
+import database from '../db/Index';
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+dotenv.config();
+
+// create a token
+async function createToken(data) {
+    const token = await jwt.sign({
+        user_id: data.user_id,
+        user_password: data.user_password,
+        user_role: data.user_role,
+        user_email: data.user_email,
+        user_name: data.user_name
+    }, process.env.User_Secret, {
+            expiresIn: 4000000000000 // expires in 1 hour
+        });
+    return token;
+}
 
 export default class Controller {
+    async signup(req, res) {
+        const hashedPassword = await bcrypt.hashSync(req.body.user_password, 10);
+        const newUser = await model.populate(req, hashedPassword);
+        // Save User Data
+        const command = `INSERT INTO
+        user_accounts(user_name,user_role,user_email,user_password)
+          VALUES($1, $2, $3, $4) returning *`;
+        const { rows } = await database.query(command, newUser);
+        const token = await createToken(rows[0]);
+        return res.status(201).send({
+            status: 'User Sent Successfully',
+            auth: "true",
+            token: token,
+            User_sent: rows[0]
+        });
+    };
+
+    async login(req, res) {
+        const command = 'SELECT * FROM user_accounts WHERE user_email=$1';
+        const { rows } = await database.query(command, [req.body.user_email]);
+        if (!rows[0]) {
+            return res.status(404).send({
+                success: 'false',
+                status: 'User Not Found in the Database'
+            });
+        }
+        const passwordIsValid = await bcrypt.compareSync(req.body.user_password, rows[0].user_password);
+        if (!passwordIsValid) return res.status(401).send({ auth: "false", token: null });
+        const token = await createToken(req.body);
+        res.status(200).send({ auth: "true", token: token, message: "Login Successful", user: rows[0] });
+    }
+
     /**
         * Gets All user_accounts in the database and sends as response
         * @param {*} req - incomming request data
@@ -10,21 +60,13 @@ export default class Controller {
     */
     async getUsers(req, res) {
         const command = 'SELECT * FROM user_accounts';
-        try {
-            const { rows, rowCount } = await database.query(command);
-            return res.status(200).send({
-                success: 'true',
-                status: 'Food Items retrieved successfully',
-                user_accounts: rows,
-                total_user_accounts: rowCount
-            });
-        } catch (error) {
-            return res.status(400).send({
-                success: 'false',
-                status: 'Bad Request',
-                message: 'Users Not Found'
-            });
-        }
+        const { rows, rowCount } = await database.query(command);
+        return res.status(200).send({
+            success: 'true',
+            status: 'Users Data retrieved successfully',
+            user_accounts: rows,
+            total_user_accounts: rowCount
+        });
     }
 
     /**
@@ -34,25 +76,17 @@ export default class Controller {
     */
     async getUser(req, res) {
         const command = 'SELECT * FROM user_accounts WHERE user_id=$1';
-        try {
-            const { rows } = await database.query(command, [req.params.userId]);
-            if (!rows[0]) {
-                return res.status(404).send({
-                    success: 'false',
-                    status: 'User Not Found in the Database'
-                });
-            } else return res.status(200).send({
-                success: 'true',
-                status: 'User retrieved successfully',
-                User: rows[0]
-            });
-        } catch (error) {
-            return res.status(400).send({
+        const { rows } = await database.query(command, [req.params.userId]);
+        if (!rows[0]) {
+            return res.status(404).send({
                 success: 'false',
-                status: 'Bad Request',
-                message: 'User Not Found'
+                status: 'User Not Found in the Database'
             });
-        }
+        } else return res.status(200).send({
+            success: 'true',
+            status: 'User retrieved successfully',
+            User: rows[0]
+        });
     }
 
     /**
@@ -61,23 +95,16 @@ export default class Controller {
         *  @param {*} res - response to the sucess of the event
     */
     async addUser(req, res) {
-        let newUser = model.populate(req);
+        const hashedPassword = req.body.user_password;
+        let newUser = model.populate(req, hashedPassword);
         const command = `INSERT INTO
     user_accounts(user_name,user_role,user_email,user_password)
       VALUES($1, $2, $3, $4) returning *`;
-        try {
-            const { rows } = await database.query(command, newUser);
-            return res.status(201).send({
-                User_sent: rows[0],
-                status: 'User Sent Successfully'
-            });
-        } catch (error) {
-            return res.status(400).send({
-                success: 'false',
-                status: 'Bad Request',
-                message: error
-            });
-        }
+        const { rows } = await database.query(command, newUser);
+        return res.status(201).send({
+            User_sent: rows[0],
+            status: 'User Sent Successfully'
+        });
     }
 
     /**
@@ -86,35 +113,27 @@ export default class Controller {
      * @param {*} res - response to the success of the event 
      */
     async updateUser(req, res) {
-        let user = model.populate(req);
+        const hashedPassword = req.body.user_password;
+        let user = model.populate(req, hashedPassword);
         let date = new Date();
         user.push(date);
         user.push(req.params.userId);
         const findQuery = `SELECT * FROM user_accounts WHERE user_id=$1`;
         const updateQuery = `UPDATE user_accounts SET user_name=$1,user_role=$2,user_email=$3,user_password=$4, modified_date=$5 WHERE user_id=$6 returning *`;
-        try {
-            const { rows } = await database.query(findQuery, [req.params.userId]);
-            if (!rows[0]) {
-                return res.status(410).send({
-                    success: 'false',
-                    status: 'Requested resourse is no longer available'
-                });
-            }
-            const response = await database.query(updateQuery, user);
-            return res.status(200).send({
-                userId: req.params.userId,
-                old_user: rows[0],
-                update: response.rows[0],
-                status: "Update successful"
-            });
-        } catch (error) {
-            console.log(error);
-            return res.status(400).send({
+        const { rows } = await database.query(findQuery, [req.params.userId]);
+        if (!rows[0]) {
+            return res.status(410).send({
                 success: 'false',
-                status: 'Bad Request',
-                message: 'User Not Found'
+                status: 'Requested resourse is no longer available'
             });
         }
+        const response = await database.query(updateQuery, user);
+        return res.status(200).send({
+            userId: req.params.userId,
+            old_user: rows[0],
+            update: response.rows[0],
+            status: "Update successful"
+        });
     }
 
     /**
@@ -124,24 +143,16 @@ export default class Controller {
     */
     async deleteUser(req, res) {
         const deleteQuery = 'DELETE FROM user_accounts WHERE user_id=$1 returning *';
-        try {
-            const { rows } = await database.query(deleteQuery, [req.params.userId]);
-            if (!rows[0]) {
-                return res.status(404).send({
-                    success: 'false',
-                    status: 'User Not Found in the Database'
-                });
-            }
-            return res.status(200).send({
-                success: 'true',
-                status: 'User deleted successfuly'
-            });
-        } catch (error) {
-            return res.status(400).send({
+        const { rows } = await database.query(deleteQuery, [req.params.userId]);
+        if (!rows[0]) {
+            return res.status(404).send({
                 success: 'false',
-                status: 'Bad Request',
-                message: "User Not Found"
+                status: 'User Not Found in the Database'
             });
         }
+        return res.status(200).send({
+            success: 'true',
+            status: 'User deleted successfuly'
+        });
     }
 }
