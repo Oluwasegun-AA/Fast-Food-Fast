5d6e87963d9e5374fa950df3095889ea0eb6d6a1
shegsteham <oluwasegunadepoju@gmail.com>
2018-11-03 23:36:16 +0100

    Authenticate users

--- client/src/auth.js
+++ client/src/auth.js
@@ -2,6 +2,8 @@ import Controller from './controller';
 let controller = new Controller('https://fast-food-fast1.herokuapp.com/api/v1');
 
 export default async (link) => {
+    let url = `https://fast-food-fast1.herokuapp.com${link}`;
+    let adminUrl= `https://fast-food-fast1.herokuapp.com/admin.html`;
     let token = localStorage.getItem('token');
     let method = {
         method: 'POST',
@@ -9,23 +11,24 @@ export default async (link) => {
             'Accept': 'text/plain, application/json, */*',
             'Content-type': 'application/json',
             'x-access-token': token
-        },
-    };
-    if (token !== null) {
-        try {
-            let data = await controller.post('/auth/', method);
-            if (data.auth === "true") {
-                localStorage.setItem('token', data.token);
-            }
-            window.location.replace('https://fast-food-fast1.herokuapp.com/front-page.html');
-            // window.location.href(link);
-        } catch (err) {
-            if (err) {
-                window.location.replace('https://fast-food-fast1.herokuapp.com/');
-                // window.location.href('./index.html');
-            }
         }
-     } else 
-     window.location.replace('https://fast-food-fast1.herokuapp.com/');
-    //  window.location.href('./index.html');
+    };
+    try {
+        console.log(token);
+        const data = await controller.post('/auth', method);
+        console.log(data);
+    //     if (data.auth === "true") {
+    //         localStorage.setItem('token', data.token);
+    //     }
+    //     if (data.user.user_role === "User"){
+    //     window.location.replace(url);
+    // } else window.location.replace(adminUrl);
+        // window.location.href(link);
+    } catch (err) {
+        if (err) {
+            console.log(data);
+            // window.location.replace('https://fast-food-fast1.herokuapp.com/');
+            // window.location.href('./index.html');
+        }
+    }
 }
